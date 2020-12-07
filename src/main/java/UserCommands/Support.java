efd031badf0deeec9576ff7f98f72b30ebedde55
ikanrai <joelmyrday@hotmail.com>
2020-12-07 15:28:54 -0500

    Updated the bot activity to a file on my desktop and made the support server command

---
+++ src/main/java/UserCommands/Support.java
@@ -0,0 +1,21 @@
+package UserCommands;
+
+import Command.Command;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.TextChannel;
+
+public class Support extends Command {
+
+    public Support(DiscordApi api) {
+        super(api);
+        api.addMessageCreateListener(event ->
+                requestSupport(super.getApi(), super.getChannel()));
+    }
+
+    private void requestSupport(DiscordApi api, TextChannel channel) {
+        if (!onCommand()) {
+            return;
+        }
+        channel.sendMessage("Here's the invite to the support server: https://discord.gg/Zpr5qTzKmd");
+    }
+}
