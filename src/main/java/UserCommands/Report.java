ded810c483a543f51cc84788446fdb6937af057d
ikanrai <joelmyrday@hotmail.com>
2020-12-09 14:38:25 -0500

    Figured out the way to add reactions is with the actual unicode string \u[string] and it works like a charm now. Updated the message sent on a failed request with the proper usage

--- src/main/java/UserCommands/Report.java
+++ src/main/java/UserCommands/Report.java
@@ -4,6 +4,7 @@ import Command.Command;
 import lombok.Getter;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
 
 import java.util.List;
@@ -15,15 +16,16 @@ public class Report extends Command {
     public Report(DiscordApi api) {
         super(api);
         api.addMessageCreateListener(event ->
-            reportCommand(api, super.getMessageAuthor(), super.getChannel(), super.getArgs()));
+            reportCommand(api, super.getMessageAuthor(), super.getMessage(), super.getChannel(), super.getArgs()));
     }
 
-    private void reportCommand(DiscordApi api, MessageAuthor author, TextChannel channel, List<String> args) {
+    private void reportCommand(DiscordApi api, MessageAuthor author, Message message, TextChannel channel, List<String> args) {
         if (!onCommand()) {
             return;
         }
         if (args.size() == 0) {
-            channel.sendMessage("Please explain the issue you are encountering");
+            message.addReaction("\uD83D\uDEAB").join();
+            channel.sendMessage("Please explain the issue you are encountering: " + getKey() + " [reason]").join();
             return;
         }
 
@@ -31,7 +33,7 @@ public class Report extends Command {
         for (String s : args) {
             msg.append(s + " ");
         }
-
+        message.addReaction("\u2705");
         api.getServerById("692871508590067823")
                 .get()
                 .getChannelById("704097798102057071")
