86bf5c24085ae29719adf5bd2bd628d89c19e77a
ikanrai <joelmyrday@hotmail.com>
2020-12-23 15:02:55 -0500

    refactored the microwave repsonse. Nothing really changed other than it's super short now

--- src/main/java/Listener/MicrowaveResponse.java
+++ src/main/java/Listener/MicrowaveResponse.java
@@ -1,51 +1,23 @@
 package Listener;
 
+import Command.Command;
 import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 
-public class MicrowaveResponse 
-{
-	private String triggerPhrase = "microwave";
-	private String responseLink = "https://www.youtube.com/watch?v=js71WSAos5M&t=25s";
+public class MicrowaveResponse extends Command {
 	
-	public MicrowaveResponse(DiscordApi getApi)
-	{
-		DiscordApi microApi = getApi;
+	public MicrowaveResponse(DiscordApi api) {
+		super(api);
 		
-		listenMicrowave(microApi);
-		
-		System.out.println("MicrowaveResponse.java loaded!");
+		api.addMessageCreateListener(event ->
+				listenMicrowave(super.getChannel(), super.getMessage()));
+
 	}
 	
-	public void listenMicrowave(DiscordApi getApi)
-	{
-		DiscordApi microListApi = getApi;
-		
-		microListApi.addMessageCreateListener(event ->
-		{
-			String messageToSplit;
-			String[] splitMessage;
-			
-			if (event.getMessageAuthor().isUser())
-			{
-				try
-				{
-					messageToSplit = event.getMessageContent();
-					messageToSplit = messageToSplit.toLowerCase();
-					splitMessage = messageToSplit.split(" ");
-					
-					for (int i = 0; i < splitMessage.length; ++i)
-					{
-						if (splitMessage[i].equals(triggerPhrase))
-						{
-							event.getChannel().sendMessage(responseLink);
-						}
-					}
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
+	public void listenMicrowave(TextChannel channel, Message message) {
+		if (message.getContent().equalsIgnoreCase("microwave")) {
+			channel.sendMessage("https://www.youtube.com/watch?v=js71WSAos5M&t=25s");
+		}
 	}
 }
