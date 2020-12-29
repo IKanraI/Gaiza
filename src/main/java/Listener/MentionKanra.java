85505563304b421cf46bd071a4cb1f2820ececa5
IKanraI <joelmyrday@hotmail.com>
2020-12-29 05:11:02 -0500

    Forgot to change a file path. Need to refactor the mention kanra thing still

--- src/main/java/Listener/MentionKanra.java
+++ src/main/java/Listener/MentionKanra.java
@@ -33,7 +33,7 @@ public class MentionKanra {
 
 
             try {
-                FileWriter file = new FileWriter("C:\\Users\\17244\\Desktop\\Mentions.txt", true);
+                FileWriter file = new FileWriter("C:\\Users\\joelm\\Desktop\\Mentions.txt", true);
                 StringBuilder message = new StringBuilder();
                 message.append(event.getMessage().getAuthor().getDisplayName()
                         + ": " + event.getMessageContent()
