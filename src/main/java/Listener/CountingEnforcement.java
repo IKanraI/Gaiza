599e92ea6506e683cc364320e3095df7c14ec585
Will Barnwell <mrwikz@gmail.com>
2021-06-30 16:40:33 -0600

    Patch exploit and some cleanup
    Various cleanup activities, some comments, and a patch to the exploit I found and exploited with IceMan where editing previous messages to anything other than a pure int broke all the checks and allowed anarchy

--- src/main/java/Listener/CountingEnforcement.java
+++ src/main/java/Listener/CountingEnforcement.java
@@ -9,6 +9,9 @@ import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
 import org.javacord.api.entity.user.User;
 
+import java.lang.Exception;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Locale;
 
 public class CountingEnforcement extends Command {
@@ -21,15 +24,15 @@ public class CountingEnforcement extends Command {
     }
     @SneakyThrows
     private void countCheck(TextChannel channel, Message message, MessageAuthor author) {
-        if (author.getIdAsString().equals(BotInfo.getBotId())) {
-            return;
-        }
-        if (!channel.getIdAsString().equals("519563130603307018")) {
+        // ignore messages not in this one hardcoded channel or from the bot
+        if (!channel.getIdAsString().equals("519563130603307018") ||
+            author.getIdAsString().equals(BotInfo.getBotId())) {
             return;
         }
         String msg = message.getContent().replaceAll("\\*", "");
+        Integer msgNum;
         try {
-            Integer.parseInt(msg);
+            msgNum = parseIntFromMessage(msg);
         } catch (Exception e) {
             channel.sendMessage("<@" + author.getIdAsString() + ">" + ", did you forget what a number is?");
             Thread.sleep(1250);
@@ -39,24 +42,30 @@ public class CountingEnforcement extends Command {
             return;
         }
 
-        Integer input;
+        Integer previousNum;
         User user;
-        if (channel.getMessages(2).get().getOldestMessage().get().getAuthor().getIdAsString().equals(BotInfo.getBotId())) {
-            input = Integer.parseInt(channel.getMessages(3).get().getOldestMessage().get().getContent().replaceAll("\\*", "")) + 1;
-            user = channel.getMessages(3).get().getOldestMessage().get().getAuthor().asUser().get();
-        } else {
-            input = Integer.parseInt(channel.getMessages(2).get().getOldestMessage().get().getContent().replaceAll("\\*", "")) + 1;
-            user = channel.getMessages(2).get().getOldestMessage().get().getAuthor().asUser().get();
-        }
+        try {
+            if (channel.getMessages(2).get().getOldestMessage().get().getAuthor().getIdAsString().equals(BotInfo.getBotId())) {
+                previousNum = parseIntFromMessage(channel.getMessages(3).get().getOldestMessage().get().getContent().replaceAll("\\*", "")); 
+                user = channel.getMessages(3).get().getOldestMessage().get().getAuthor().asUser().get();
+            } else {
+                previousNum = parseIntFromMessage(channel.getMessages(2).get().getOldestMessage().get().getContent().replaceAll("\\*", ""));
+                user = channel.getMessages(2).get().getOldestMessage().get().getAuthor().asUser().get();
+            }
 
-        if (!input.equals(Integer.parseInt(msg))) {
-            channel.sendMessage("<@" + author.getIdAsString() + ">" + ", please input the correct next number");
-            Thread.sleep(1250);
-            message.delete();
-        } else if (author.getIdAsString().equals(user.getIdAsString())) {
-            channel.sendMessage("<@" + author.getIdAsString() + ">" + ", double counting isn't allowed. Please wait until someone else has counted");
-            Thread.sleep(1250);
-            message.delete();
+            if (!previousNum.equals(msgNum - 1)) {
+                channel.sendMessage("<@" + author.getIdAsString() + ">" + ", please input the correct next number");
+                Thread.sleep(1250);
+                message.delete();
+            } else if (author.getIdAsString().equals(user.getIdAsString())) {
+                channel.sendMessage("<@" + author.getIdAsString() + ">" + ", double counting isn't allowed. Please wait until someone else has counted");
+                Thread.sleep(1250);
+                message.delete();
+            }
+        } catch (BadInputException bie) {
+            // if we can't parse numbers from the previous messages
+            // trust the user and ask for help
+            channel.sendMessage("<@" + author.getIdAsString() + ">" + ", I can't read the previous input, so I am trusting you, please call my dad uwu");
         }
 
         for (Message m : channel.getMessages(10).get()) {
@@ -64,6 +73,29 @@ public class CountingEnforcement extends Command {
                 Thread.sleep(1500);
                 m.delete();
             }
+        }
+    }
+    private Integer parseIntFromMessage(String message) throws BadInputException {
+        Integer msgNum;
+        // idk if this will cause issues when I throw a null error into the Throwable
+        Exception hopefullyNot = null;
+        // https://regex101.com/r/fwHchU/1
+        Pattern beginsWithInteger = Pattern.compile("^([0-9]+)");
+        Matcher matchBeginsWithInt = pattern.matcher(message);
+        if (matchBeginsWithInt.find()) {
+            try {
+                msgNum = Integer.parseInt(matchBeginsWithInt.group(1));
+            } catch (Exception e) {
+                throw new BadInputException("Somehow failed to parse a series of numbers as an Int", e);
+            }
+        } else {
+            throw new BadInputException("User input did not begin with an int", hopefullyNot);
+        }
+            
+    }
+    private class BadInputException extends Exception {
+        public BadInputException(String errorMessage, Throwable err) {
+            super(errorMessage, err);
         }
     }
 }
