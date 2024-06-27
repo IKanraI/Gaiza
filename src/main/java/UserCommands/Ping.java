e594ed0906d9e08965400db8e07d96d8f5834d14
IKanraI <joelmyrday@gmail.com>
2024-06-27 13:58:13 -0400

    Implemented slash command for ping

--- src/main/java/UserCommands/Ping.java
+++ src/main/java/UserCommands/Ping.java
@@ -1,26 +1,30 @@
 package UserCommands;
 
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.javacord.api.DiscordApi;
 import Command.*;
 
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.interaction.Interaction;
 
-public class Ping extends Command {
+public class Ping {
 	@Getter
 	public static String help = "Typical test command. Returns pong normally.";
-	
+
 	public Ping(DiscordApi api) {
-		super(api);
-		api.addMessageCreateListener(e ->
-			pingCommand(super.getChannel(), super.getMessageAuthor()));
+		//super(api);
+		api.addSlashCommandCreateListener(e ->
+			pingCommand(e.getSlashCommandInteraction().getFullCommandName(), e.getSlashCommandInteraction().getUser().getIdAsString(), e.getInteraction()));
 	}
 
-	private void pingCommand(TextChannel channel, MessageAuthor messageAuthor) {
-		if(!onCommand()) {
+	private void pingCommand(String command, String messageAuthorId, Interaction event) {
+		if (!StringUtils.equalsIgnoreCase(command, "ping"))
 			return;
-		}
-		channel.sendMessage("<@" + messageAuthor.getIdAsString() + "> rules! ... well pong i guess");
+
+		event.createImmediateResponder()
+				.setContent("<@" + messageAuthorId + "> rules! ... well pong i guess")
+				.respond();
 	}
 }
